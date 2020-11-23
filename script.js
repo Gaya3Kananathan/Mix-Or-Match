e5b95c8784dde3295614666dba6196a7e4263ada
Gayathri S K <gayathrikananathan@gmail.com>
2020-11-23 08:03:22 +0530

    Change time

--- script.js
+++ script.js
@@ -138,7 +138,7 @@ class MixOrMatch {
 function ready() {
     let overlays = Array.from(document.getElementsByClassName('overlay-text'));
     let cards = Array.from(document.getElementsByClassName('card'));
-    let game = new MixOrMatch(100, cards);
+    let game = new MixOrMatch(50, cards);
 
     overlays.forEach(overlay => {
         overlay.addEventListener('click', () => {
